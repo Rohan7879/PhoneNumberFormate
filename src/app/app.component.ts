61f53ac0b7139aec6973423c4e590715514ee16c
Rohan Vasoya <rohanvasoya@Rohans-MacBook-Air.local>
2023-09-20 12:15:59 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'numberFor';
+  phoneNumber: string = '';
+
+  numberFormate() {
+    const cleanNumber = this.phoneNumber.replace(/\D/g, '');
+
+    if (cleanNumber.length >= 10) {
+      const logic = `(${cleanNumber.slice(0, 3)})-${cleanNumber.slice(
+        3,
+        6
+      )}-${cleanNumber.slice(6, 10)}`;
+      this.phoneNumber = logic;
+    } else {
+      alert('Please enter valid number');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
